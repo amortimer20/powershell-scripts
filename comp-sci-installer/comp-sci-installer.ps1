5041bca9af61b521e11a9c72948e1cdf618bb8b7
Anthony Mortimer <agm20@outlook.com>
2024-08-09 13:09:14 -0400

    Initial commit

---
+++ comp-sci-installer/comp-sci-installer.ps1
@@ -0,0 +1,16 @@
+﻿# Winget packages
+winget install Python.Python.3.12 --scope machine
+winget install Microsoft.VisualStudioCode --scope machine
+
+# Reload path
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
+
+# Packages & Extensions
+pip install raylib
+pip install rich
+code --extensions-dir "%ProgramFiles%\Microsoft VS Code\resources\app\extensions" --install-extension ms-python.python --force
+
+# Installers
+Set-Location $PSScriptRoot
+.\VisualStudioSetup.exe
+.\UnityHubSetup.exe
