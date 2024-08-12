85410d42f08365398fdf2db9c012093c66909c46
Anthony Mortimer <agm20@outlook.com>
2024-08-12 13:57:35 -0400

    Add messages before each action

--- comp-sci-installer/comp-sci-installer.ps1
+++ comp-sci-installer/comp-sci-installer.ps1
@@ -1,16 +1,27 @@
-﻿# Winget packages
+﻿Write-DelayedOutput "Installing Python v3.12 and VS Code"
 winget install Python.Python.3.12 --scope machine
 winget install Microsoft.VisualStudioCode --scope machine
 
 # Reload path
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
-# Packages & Extensions
+Write-DelayedOutput "Installing Python packages and the Python extension for VS Code"
 pip install raylib
 pip install rich
 code --extensions-dir "%ProgramFiles%\Microsoft VS Code\resources\app\extensions" --install-extension ms-python.python --force
 
-# Installers
+Write-DelayedOutput "Running installers; See 'instructions.txt'"
 Set-Location $PSScriptRoot
 .\VisualStudioSetup.exe
 .\UnityHubSetup.exe
+
+function Write-DelayedOutput {
+    param (
+        [Parameter(Position=0)]
+        [string]$Message,
+        [double]$DelayTime = 3
+    )
+
+    Write-Host $Message -ForegroundColor Green
+    Start-Sleep $DelayTime
+}
