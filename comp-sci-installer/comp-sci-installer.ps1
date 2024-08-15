ad46533adaf680a3cd207bda63eb37f59b173b62
Anthony Mortimer <agm20@outlook.com>
2024-08-15 09:29:36 -0400

    Chill out with the DRY code

--- comp-sci-installer/comp-sci-installer.ps1
+++ comp-sci-installer/comp-sci-installer.ps1
@@ -1,27 +1,19 @@
-﻿Write-DelayedOutput "Installing Python v3.12 and VS Code"
+﻿Write-Host "Installing Python v3.12 and VS Code" -ForegroundColor Green
+Start-Sleep 3
 winget install Python.Python.3.12 --scope machine
 winget install Microsoft.VisualStudioCode --scope machine
 
 # Reload path
 $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 
-Write-DelayedOutput "Installing Python packages and the Python extension for VS Code"
+Write-Host "Installing Python packages and the Python extension for VS Code" -ForegroundColor Green
+Start-Sleep 3
 pip install raylib
 pip install rich
 code --extensions-dir "%ProgramFiles%\Microsoft VS Code\resources\app\extensions" --install-extension ms-python.python --force
 
-Write-DelayedOutput "Running installers; See 'instructions.txt'"
+Write-Host "Running installers (See 'instructions.txt')" -ForegroundColor Green
+Start-Sleep 3
 Set-Location $PSScriptRoot
 .\VisualStudioSetup.exe
 .\UnityHubSetup.exe
-
-function Write-DelayedOutput {
-    param (
-        [Parameter(Position=0)]
-        [string]$Message,
-        [double]$DelayTime = 3
-    )
-
-    Write-Host $Message -ForegroundColor Green
-    Start-Sleep $DelayTime
-}
